3e94dba5d6b8fa16392d36a39f1ed9546c659002
Milo Bonnet <milovtt@gmail.com>
2022-12-05 14:10:03 +0100

    Cube

--- 3.2 Events/script.js
+++ 3.2 Events/script.js
@@ -4,10 +4,20 @@ const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
+
 const clickOnSquare = (e) => {
-  console.log(e.target.classList[1])
-  console.log(getElapsedTime())
+    console.log(e.target.classList[1])
+    console.log(getElapsedTime())
+
+    section = document.getElementsByTagName('section') [0];
+
+    let div = document.createElement('div')
+    div.classList.add("displayedsquare", e.target.classList[1])
+    section.appendChild(div);
 }
+
+
+
 
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
